e0c3d686388b4dc45ca6c3652e586bfbc2a9f813
Colmeia Team <team@colmeia.dev>
2026-02-17 00:20:57 -0300

    Forçar deploy ignorando erros

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   typescript: {
+    // Isso permite que o deploy termine mesmo com erros de TypeScript
     ignoreBuildErrors: true,
   },
   eslint: {
+    // Evita que o build pare por avisos de formatação
     ignoreDuringBuilds: true,
   },
 };
